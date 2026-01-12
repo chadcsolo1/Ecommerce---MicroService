37ddf487ce2a598511be9303b01db6f9904b6495
chad solomon <chadcsoloman@gmail.com>
2026-01-12 07:58:03 -0500

    Adding DatabaseSeeder class

---
+++ Catalog/Data/DatabaseSeeder.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Data
+{
+
+    public class DatabaseSeeder
+    {
+        
+        public static async Task SeedAsync(IConfiguration config)
+        {
+            var client = new MongoClient(config["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(config["DatabaseSettings:DatabaseName"]);
+            var productCollection = database.GetCollection<Product>(config["DatabaseSettings:ProductCollectionName"]);
+            var brandCollection = database.GetCollection<ProductBrand>(config["DatabaseSettings:BrandCollectionName"]);
+            var typeCollection = database.GetCollection<ProductType>(config["DatabaseSettings:TypeCollectionName"]);
+
+            var seedBasePath = Path.Combine("Data", "SeedData");
+
+            // Seed Brands
+            List<ProductBrand>  brandList = new();
+
+            if (await brandCollection.CountDocumentsAsync(p => true) == 0)
+            {
+                //var brandData = await File.ReadAllTextAsync(seedBasePath, "brands.json");
+            }
+            
+        }
+    }
+}
