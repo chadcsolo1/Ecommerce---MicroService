05d1248d59946c7a0ec5ed258e627b96e5a37b7c
chad solomon <chadcsoloman@gmail.com>
2026-01-12 22:03:27 -0500

    Working on database seeder class.

--- Catalog/Data/DatabaseSeeder.cs
+++ Catalog/Data/DatabaseSeeder.cs
@@ -1,5 +1,6 @@
 ﻿using Catalog.Entities;
 using MongoDB.Driver;
+using System.Text.Json;
 
 namespace Catalog.Data
 {
@@ -20,9 +21,56 @@ namespace Catalog.Data
             // Seed Brands
             List<ProductBrand>  brandList = new();
 
-            if (await brandCollection.CountDocumentsAsync(p => true) == 0)
+            //brandCollection database is empty then seed data else get all data from database
+            if ((await brandCollection.CountDocumentsAsync(p => true)) == 0)
             {
-                //var brandData = await File.ReadAllTextAsync(seedBasePath, "brands.json");
+                //Read all data from brands.json, deserialize and insert to brandCollection database
+                var brandData = await File.ReadAllTextAsync(Path.Combine(seedBasePath, "brands.json"));
+                brandList = JsonSerializer.Deserialize<List<ProductBrand>>(brandData);
+                await brandCollection.InsertManyAsync(brandList);
+            } else
+            {
+                brandList = await brandCollection.Find(p => true).ToListAsync();
+            }
+
+            // Seed Types
+            List<ProductType> typeList = new();
+
+            //typeCollection database is empty then seed data else get all data from database
+            if ((await typeCollection.CountDocumentsAsync(p => true)) == 0)
+            {
+                //Read all data from types.json, deserialize and insert to typeCollection database
+                var typeData = await File.ReadAllTextAsync(Path.Combine(seedBasePath, "types.json"));
+                typeList = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+                await typeCollection.InsertManyAsync(typeList);
+            } else
+            {
+                typeList = await typeCollection.Find(p => true).ToListAsync();
+            }
+
+            // Seed products
+            List<Product> productList = new();  
+
+            //productCollection database is empty then seed data else get all data from database
+            if ((await productCollection.CountDocumentsAsync(p => true)) == 0)
+            {
+                //Read all data from products.json, deserialize and insert to productCollection database
+                var productData = await File.ReadAllTextAsync(Path.Combine(seedBasePath, "products.json"));
+                productList = JsonSerializer.Deserialize<List<Product>>(productData);
+                foreach (var product in productList)
+                {
+                    //Reset Id to have Mongo generate new Id
+                    product.Id = null;
+                    if (product.CreatedDate == default)
+                    {
+                        product.CreatedDate = DateTimeOffset.UtcNow;
+                    }
+
+                }
+                await productCollection.InsertManyAsync(productList);
+            } else
+            {
+                productList = await productCollection.Find(p => true).ToListAsync();
             }
             
         }
