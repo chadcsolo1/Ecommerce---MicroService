d7acc27d4632a4cede2d40db717572eb38c69276
chadcsolo1 <solomonchad100@gmail.com>
2026-02-19 20:53:17 -0500

    Created Order Web API and the base and order entity

---
+++ Order/Entities/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Order.Entities
+{
+    public abstract class EntityBase
+    {
+        //protected set is made to use in the derived class
+        public int Id { get; protected set; }
+        //few audit props
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime? CreatedDate  { get; set; }
+        public string LastModifiedBy { get; set; } = string.Empty;
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
---
+++ Order/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace Order.Entities
+{
+    public class Order : EntityBase
+    {
+        public string UserName { get; set; } = string.Empty;
+        public decimal? TotalPrice { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+        public string CardName { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string Expiration { get; set; } = string.Empty;
+        public string Cvv { get; set; } = string.Empty;
+        public int? PaymentMethod { get; set; }
+    }
+}
