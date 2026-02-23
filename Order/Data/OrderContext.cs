444646303c30bb2aaa42c6e2da43cd0d449dc1c6
chadcsolo1 <solomonchad100@gmail.com>
2026-02-22 20:59:54 -0500

    Adding IAsyncRepository and Order Context to Orders.

---
+++ Order/Data/OrderContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Order.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+        public DbSet<Entities.Order> Orders { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+}
